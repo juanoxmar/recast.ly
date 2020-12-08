3a443694ffe0275b6327bac733a3564c86020788
Juan Oxmar <joxmar.ramirez@gmail.com>
2020-12-07 21:16:25 -0800

    Complete up to VideoPlayer

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,17 +1,16 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {props.videos.map((item, index) => <VideoListEntry key={index} videoIndex={index} video={item} vidUpdate={props.vidUpdate} />)}
   </div>
 );
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
-  videos: PropTypes.array.isRequired
+  videos: PropTypes.array.isRequired,
+  vidUpdate: PropTypes.func
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
