76fe1f154ecfd5f1929a136426c599392154a88b
Juan Oxmar <joxmar.ramirez@gmail.com>
2020-12-08 16:29:16 -0800

    Functional recast.ly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,36 +2,66 @@ import Search from './Search.js';
 import VideoPlayer from './VideoPlayer.js';
 import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
+import searchYouTube from '../lib/searchYouTube.js';
+import API_KEY from '../config/youtube.js';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
 
     this.state = {
-      videoId: 0
+      videoId: 0,
+      data: null
     };
+  }
+
+  componentDidMount() {
+    searchYouTube({
+      query: 'react javascript',
+      max: 5,
+      key: API_KEY,
+    }, this.saveData.bind(this));
   }
 
   newVideoClickHandler(num) {
     this.setState({
-      videoId: num
+      videoId: num,
     });
   }
 
+  saveData(data) {
+    this.setState({
+      data: data
+    });
+  }
+
+  searchBar(string) {
+    searchYouTube({
+      query: string,
+      max: 5,
+      key: API_KEY,
+    }, this.saveData.bind(this));
+  }
+
   render() {
+    let loading = true;
+    const loadingTag = <div>Loading</div>;
+    if (this.state.data) {
+      loading = false;
+    }
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <Search />
+            <Search searchFunc={this.searchBar.bind(this)} />
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <VideoPlayer video={exampleVideoData[this.state.videoId]} />
+            {loading ? loadingTag : <VideoPlayer video={this.state.data[this.state.videoId]} />}
           </div>
           <div className="col-md-5">
-            <VideoList videos={exampleVideoData} vidUpdate={this.newVideoClickHandler.bind(this)} />
+            {loading ? loadingTag : <VideoList videos={this.state.data} vidUpdate={this.newVideoClickHandler.bind(this)} />}
           </div>
         </div>
       </div>
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,38 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      q: options.query,
+      maxResults: options.max,
+      key: options.key,
+      part: 'snippet',
+    },
+    dataType: 'json',
+    success: data => {
+      callback(data.items);
+    },
+    error: () => {
+      console.error('Request Failed');
+    }
+  });
 };
 
 export default searchYouTube;
+
+// get(url_settings?: string | JQuery<TElement = HTMLElement>.UrlAjaxSettings<any>)
+
+// {
+//   "kind": "youtube#searchListResponse",
+//   "etag": etag,
+//   "nextPageToken": string,
+//   "prevPageToken": string,
+//   "regionCode": string,
+//   "pageInfo": {
+//     "totalResults": integer,
+//     "resultsPerPage": integer
+//   },
+//   "items": [
+//     search Resource
+//   ]
+// }
